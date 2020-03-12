b80d3b40a9c3a00727a0912dbabfb3c07d5fa40a
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-03-12 04:05:12 +0000

    Category Datatable: added the ajax call to getall and delete API and added html on teh JS file

--- Uplift/Uplift/wwwroot/js/category.js
+++ Uplift/Uplift/wwwroot/js/category.js
@@ -1,1 +1,37 @@
-﻿
+﻿var dataTabel;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTabel = $('#tblData').DataTable({
+        "ajax": {
+            "url": "admin/category/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            { "data": "displayOrder", "width": "20%" },
+            {
+                "data": "Id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/category/Upsert/${data}"class='btn btn-success text-white' style='cursor:pointer; width:100pxl;'>
+                                <i class='far fa-edit'></i> Edit
+                                </a>
+                                &nbsp;
+                                <a onClick= Delete("/Admin/category/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100pxl;'>
+                                <i class='far fa-trash-alt'></i> Delete
+                                </a>
+                            </div>`;
+                }, "width": "30%"
+            }
+        ], "language": {
+            "emptyTable":"No records found."
+        },
+        "width":"100%"
+    });
+
+};
