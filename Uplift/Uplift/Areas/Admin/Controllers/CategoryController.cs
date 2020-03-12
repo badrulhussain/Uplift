ceaebff4b74d7c187d45a18fdea542aecc9444ee
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-03-12 02:04:32 +0000

    Update Category Index View and cleaned the layout

--- Uplift/Uplift/Areas/Admin/Controllers/CategoryController.cs
+++ Uplift/Uplift/Areas/Admin/Controllers/CategoryController.cs
@@ -7,6 +7,7 @@ using Uplift.DataAccess.Data.Repository.IRepository;
 
 namespace Uplift.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
