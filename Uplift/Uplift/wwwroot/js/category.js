bc01cc9c13616cf28c1367f8531bf97489ee96a3
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-03-23 19:32:49 +0000

    Delete Category

--- Uplift/Uplift/wwwroot/js/category.js
+++ Uplift/Uplift/wwwroot/js/category.js
@@ -1,11 +1,12 @@
-﻿var dataTabel;
+﻿var dataTable;
 
 $(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    dataTabel = $('#tblData').DataTable({
+
+    dataTable = $('#tblData').DataTable({
         "ajax": {
             "url": "/admin/category/GetAll",
             "type": "GET",
@@ -15,23 +16,50 @@ function loadDataTable() {
             { "data": "name", "width": "50%" },
             { "data": "displayOrder", "width": "20%" },
             {
-                "data": "Id",
+                "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                                <a href="/Admin/category/Upsert/${data}"class='btn btn-success text-white' style='cursor:pointer; width:100pxl;'>
-                                <i class='far fa-edit'></i> Edit
+                                <a href="/Admin/category/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-edit'></i> Edit
                                 </a>
                                 &nbsp;
-                                <a onClick= Delete("/Admin/category/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100pxl;'>
-                                <i class='far fa-trash-alt'></i> Delete
+                                <a onclick=Delete("/Admin/category/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-trash-alt'></i> Delete
                                 </a>
-                            </div>`;
+                            </div>
+                            `;
                 }, "width": "30%"
             }
-        ], "language": {
-            "emptyTable":"No records found."
+        ],
+        "language": {
+            "emptyTable": "No records found."
         },
-        "width":"100%"
+        "width": "100%"
     });
+}
 
-};
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the content!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
+}
