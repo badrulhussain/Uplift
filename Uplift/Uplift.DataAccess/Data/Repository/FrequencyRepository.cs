f1f7a1c6c72a370cf626d33f5becf031cbf1b88b
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-03-27 03:29:23 +0000

    Created FrequencyRepositoryand the interface

---
+++ Uplift/Uplift.DataAccess/Data/Repository/FrequencyRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    public class FrequencyRepository : Repository<Frequency>, IFrequencyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FrequencyRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetFrequencyListForDropDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFrequency(Frequency frequencyR)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
