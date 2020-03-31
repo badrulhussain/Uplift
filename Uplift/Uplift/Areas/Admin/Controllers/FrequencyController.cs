99ec993fd47e180754b4ea96cdfb68370efd2608
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-03-31 20:54:03 +0100

    Added Frequency Controller, Index view and get all API

---
+++ Uplift/Uplift/Areas/Admin/Controllers/FrequencyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FrequencyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWord;
+
+        public FrequencyController(IUnitOfWork unitOfWord)
+        {
+            _unitOfWord = unitOfWord;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWord.FrequencyRepository.GetAll() });
+        }
+        #endregion
+    }
+}
