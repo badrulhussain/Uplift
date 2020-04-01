9551bc0b403aa96c6028a4f6d7dac354a5ab76e3
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-04-01 15:12:51 +0100

    Added FrequencyController Upsert action methods

--- Uplift/Uplift/Areas/Admin/Controllers/FrequencyController.cs
+++ Uplift/Uplift/Areas/Admin/Controllers/FrequencyController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
 
 namespace Uplift.Areas.Admin.Controllers
 {
@@ -20,6 +21,46 @@ namespace Uplift.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            var Frequency = new Frequency();
+            if(id == null)
+            {
+                return View(Frequency);
+            }
+
+            Frequency = _unitOfWord.FrequencyRepository.Get(id.GetValueOrDefault());
+            if(Frequency == null)
+            {
+                return NotFound();
+            }
+
+            return View(Frequency);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Upsert(Frequency frequency)
+        {
+            if(ModelState.IsValid)
+            {
+                if (frequency.Id == 0)
+                {
+                    _unitOfWord.FrequencyRepository.Add(frequency);
+                }
+                else
+                {
+                    _unitOfWord.FrequencyRepository.UpdateFrequency(frequency);
+                }
+
+                _unitOfWord.Save();
+
+                return View(nameof(Index));
+            }
+
+            return View(frequency);
         }
 
         #region API
