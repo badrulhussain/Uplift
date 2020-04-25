23317b531f648813b5127ef4d5bac681775e8cf3
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-04-25 18:42:27 +0100

    Created Cart Controller and it's View Model

---
+++ Uplift/Uplift.Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public IList<Service> Service { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
