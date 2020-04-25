a1f798523c5e17c7f6d3e9b63cc1bf7ba9b3dc37
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-04-25 18:29:03 +0100

    Fixed typos

--- Uplift/Uplift.Models/OrderHeader.cs
+++ Uplift/Uplift.Models/OrderHeader.cs
@@ -14,14 +14,14 @@ namespace Uplift.Models
         [Required]
         public string Phone { get; set; }
         [Required]
-        public string email { get; set; }
+        public string Email { get; set; }
         [Required]
         public string Address { get; set; }
         [Required]
         public string City { get; set; }
         [Required]
         public string ZipCode { get; set; }
-        public DateTime OderDate { get; set; }
+        public DateTime OrderDate { get; set; }
         public string Status { get; set; }
         public string Comments { get; set; }
         public int ServiceCount { get; set; }
