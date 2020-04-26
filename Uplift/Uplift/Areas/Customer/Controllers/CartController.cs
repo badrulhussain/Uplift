7b1c046c1a8e28df3dd3e803f67f5c8502f16fc7
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-04-27 00:11:35 +0100

    Added Order Summary Post Action and more status constants

--- Uplift/Uplift/Areas/Customer/Controllers/CartController.cs
+++ Uplift/Uplift/Areas/Customer/Controllers/CartController.cs
@@ -66,5 +66,51 @@ namespace Uplift.Areas.Customer.Controllers
             }
             return View(CartVM);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Summary")]
+        public IActionResult SummaryPOST() 
+        {
+            // Due to BindProperty line 21 the view model in auto bined on the method signeture  
+
+            if (HttpContext.Session.GetObject<List<int>>(SD.SessionCart) != null)
+            {
+                var sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+                foreach (var serviceId in sessionList)
+                {
+                    CartVM.ServiceList.Add(_unitOfWork.Service.Get(serviceId));
+                }
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(CartVM);
+            }
+            else
+            {
+                CartVM.OrderHeader.OrderDate = DateTime.Now;
+                CartVM.OrderHeader.Status = SD.StatusSubmitted;
+                CartVM.OrderHeader.ServiceCount = CartVM.ServiceList.Count();
+                _unitOfWork.OrderHeader.Add(CartVM.OrderHeader);
+                _unitOfWork.Save();
+
+                foreach(var item in CartVM.ServiceList)
+                {
+                    OrderDetails orderDetails = new OrderDetails()
+                    {
+                        ServiceId = item.Id,
+                        OrderHeaderId = CartVM.OrderHeader.Id,
+                        ServiceName = item.Name,
+                        Price = item.Price
+                    };
+
+                    _unitOfWork.OrderDetails.Add(orderDetails);
+                }
+                    _unitOfWork.Save();
+            }
+
+            HttpContext.Session.SetObject(SD.SessionCart, new List<int>());
+            return RedirectToAction("OrderConfirmation", "Cart", new { id = CartVM.OrderHeader.Id });
+        }
     }
 }
