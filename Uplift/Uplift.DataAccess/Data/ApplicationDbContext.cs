db984ca82c5bc92293cdafb0b5c21a60c30dae95
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-05-05 05:23:07 +0100

    Created Web Images Model and added the migrations to DB

--- Uplift/Uplift.DataAccess/Data/ApplicationDbContext.cs
+++ Uplift/Uplift.DataAccess/Data/ApplicationDbContext.cs
@@ -19,6 +19,7 @@ namespace Uplift.DataAccess.Data
         public DbSet<Service> Service { get; set; }
         public DbSet<OrderHeader> OrderHeader { get; set; }
         public DbSet<OrderDetails> OrderDetails { get; set; }
-        public DbSet<ApplicationUser> ApplicationUser { get; set; }        
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<WebImages> WebImages { get; set; }
     }
 }
