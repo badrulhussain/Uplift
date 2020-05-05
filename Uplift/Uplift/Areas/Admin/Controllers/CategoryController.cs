b3aa9b479c7f64e2a9dd265dbdb51d0897cdafb3
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-05-05 21:56:41 +0100

    Fixed files to its original format

--- Uplift/Uplift/Areas/Admin/Controllers/CategoryController.cs
+++ Uplift/Uplift/Areas/Admin/Controllers/CategoryController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Uplift.DataAccess.Data;
 using Uplift.DataAccess.Data.Repository.IRepository;
 using Uplift.Models;
 using Uplift.Utility;
@@ -13,57 +12,57 @@ namespace Uplift.Areas.Admin.Controllers
 {
     [Authorize]
     [Area("Admin")]
-    public class WebImageController : Controller
+    public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public WebImageController(ApplicationDbContext _db)
+        public CategoryController(IUnitOfWork unitOfWork)
         {
-            _db = db;
+            _unitOfWork = unitOfWork;
         }
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-        //public IActionResult Upsert(int? id)
-        //{
-        //    var category = new Category();
-        //    if(id == null)
-        //    {
-        //        return View(category);
-        //    }
+        public IActionResult Upsert(int? id)
+        {
+            var category = new Category();
+            if (id == null)
+            {
+                return View(category);
+            }
 
-        //    category = _unitOfWork.Category.Get(id.GetValueOrDefault());
-        //    if(category == null)
-        //    {
-        //        return NotFound();
-        //    }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
 
-        //    return View(category);
-        //}
+            return View(category);
+        }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Upsert(Category category)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        if (category.Id == 0)
-        //        {
-        //            _unitOfWork.Category.Add(category);
-        //        }
-        //        else
-        //        {
-        //            _unitOfWork.Category.Update(category);
-        //        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
 
-        //        _unitOfWork.Save();
+                _unitOfWork.Save();
 
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(category);
-        //}
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
 
         #region API
 
@@ -73,23 +72,23 @@ namespace Uplift.Areas.Admin.Controllers
             //return Json(new { data = _unitOfWork.Category.GetAll() });
 
             // Using the storeprocedure call to get the values 
-            return Json(new { data = _db.WebImages.ToList()});
+            return Json(new { data = _unitOfWork.SP_Call.ReturnList<Category>(SD.usp_GetAllCategory, null) });
         }
 
         [HttpDelete]
         public IActionResult Delete(int Id)
         {
-            var objFromDb = _db.WebImages.Find(Id);
+            var objFromDb = _unitOfWork.Category.Get(Id);
 
-            if(objFromDb == null)
+            if (objFromDb == null)
             {
-                return Json(new { success = false, message = "Error while deleting."});
+                return Json(new { success = false, message = "Error while deleting." });
             }
 
-            _db.WebImages.Remove(objFromDb);
-            _db.SaveChanges();
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
 
-            return Json(new { success = true, message = "Delete successful."});
+            return Json(new { success = true, message = "Delete successful." });
         }
 
         #endregion
