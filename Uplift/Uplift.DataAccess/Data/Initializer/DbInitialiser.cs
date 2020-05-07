dff22d213c0958216007158a7cfd9d097a3a1fc2
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-05-07 21:50:21 +0100

    Added  DbInitializer Class Method. And took out create roles on the register page

--- Uplift/Uplift.DataAccess/Data/Initializer/DbInitialiser.cs
+++ Uplift/Uplift.DataAccess/Data/Initializer/DbInitialiser.cs
@@ -1,14 +1,44 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Uplift.Utility;
 
 namespace Uplift.DataAccess.Data.Initializer
 {
     public class DbInitialiser : IDbInitialiser
     {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitialiser(ApplicationDbContext db,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+               
+        }
+
         public void Initialise()
         {
-            throw new NotImplementedException();
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            if (_db.Roles.Any(r => r.Name == SD.Manager)) return;
         }
     }
 }
