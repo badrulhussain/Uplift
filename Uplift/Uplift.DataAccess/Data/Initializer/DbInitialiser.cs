59ff51738e86a7d3fd07564412937efd6a966e64
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-05-07 20:16:16 +0100

    Created Db Initializer Class and Interface

---
+++ Uplift/Uplift.DataAccess/Data/Initializer/DbInitialiser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.DataAccess.Data.Initializer
+{
+    public class DbInitialiser : IDbInitialiser
+    {
+        public void Initialise()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
